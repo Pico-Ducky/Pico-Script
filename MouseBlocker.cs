b39b073207c22a31847bd9f8ce8f850a4796f821
Pico-Ducky <picoduckyraspberry@gmail.com>
2025-05-11 15:46:29 +0200

    Add files via upload

---
+++ MouseBlocker.cs
@@ -0,0 +1,60 @@
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+class MouseBlocker
+{
+    private const int WH_MOUSE_LL = 14;
+    private const int HC_ACTION = 0;
+    private const int WM_MOUSEMOVE = 0x0200;
+    private const int WM_LBUTTONDOWN = 0x0201;
+    private const int WM_LBUTTONUP = 0x0202;
+    private const int WM_RBUTTONDOWN = 0x0204;
+    private const int WM_RBUTTONUP = 0x0205;
+    private const int WM_MBUTTONDOWN = 0x0207;
+    private const int WM_MBUTTONUP = 0x0208;
+
+    private static IntPtr hookID = IntPtr.Zero;
+
+    public static void Main()
+    {
+        hookID = SetHook(MouseHookCallback);
+        Console.WriteLine("Mouse input blocked. Press Enter to exit...");
+        Console.ReadLine();
+        UnhookWindowsHookEx(hookID);
+    }
+
+    private static IntPtr SetHook(LowLevelMouseProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= HC_ACTION)
+        {
+            return (IntPtr)1; // Block all mouse actions
+        }
+        return CallNextHookEx(hookID, nCode, wParam, lParam);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+}
